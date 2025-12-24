98ff5166e6c3540cd18a75913235c536dec27190
Alberto <dav.vazquez1719@gmail.com>
2025-12-24 16:23:22 -0600

    exoPlayerAdded

--- app/src/main/java/com/example/miniexoplayer/MainActivity.kt
+++ app/src/main/java/com/example/miniexoplayer/MainActivity.kt
@@ -5,16 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var player: ExoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // 1 Crear el player
+        player = ExoPlayer.Builder(this).build()
+
+        // 2 Conectar player con la UI
+        val playerView = findViewById<PlayerView>(R.id.playerView)
+        playerView.player = player
+
+        // 3 Crear el MediaItem (el video)
+        val mediaItem = MediaItem.fromUri(
+            "https://storage.googleapis.com/exoplayer-test-media-0/play.mp3"
+        )
+
+        // 4 Enviar media al player
+        player.setMediaItem(mediaItem)
+
+        // 5 Preparar
+        player.prepare()
+
+        // 6 Reproducir
+        player.play()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // 7 Liberar recursos
+        player.release()
     }
 }
